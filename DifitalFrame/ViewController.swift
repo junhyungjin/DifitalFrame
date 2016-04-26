1fb09c63f061d6cabbaafa264e2fb491dc8ab1b3
Jun Hyung Jin <junhyungjin@gmail.com>
2016-04-26 12:36:13 +0900

    Initial Commit

---
+++ DifitalFrame/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DifitalFrame
+//
+//  Created by scott.jun on 2016. 4. 26..
+//  Copyright © 2016년 Jsoft. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
