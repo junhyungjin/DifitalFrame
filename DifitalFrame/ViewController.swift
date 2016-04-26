83585d205d24dbe9828e933b1b49c029f9576a61
Jun Hyung Jin <junhyungjin@gmail.com>
2016-04-26 18:29:10 +0900

    DigitalFrame First

--- DifitalFrame/ViewController.swift
+++ DifitalFrame/ViewController.swift
@@ -9,10 +9,31 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var imgView:UIImageView!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let cuteImages = [UIImage(named: "1.jpg")!,
+                          UIImage(named: "2.jpg")!,
+                          UIImage(named: "3.jpg")!,
+                          UIImage(named: "4.jpg")!,
+                          UIImage(named: "5.jpg")!,
+                          UIImage(named: "6.jpg")!,
+                          UIImage(named: "7.jpg")!,
+                          UIImage(named: "8.jpg")!,
+                          UIImage(named: "9.jpg")!,
+                          UIImage(named: "10.jpg")!,
+                          UIImage(named: "11.jpg")!,
+                          UIImage(named: "12.jpg")!,
+                          UIImage(named: "13.jpg")!,
+                          UIImage(named: "14.jpg")!,
+                          UIImage(named: "15.jpg")!]
+        imgView.animationImages = cuteImages
+        imgView.animationDuration = 15.0
+        imgView.startAnimating()
+        
     }
 
     override func didReceiveMemoryWarning() {
